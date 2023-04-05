721c9c56b56b77acc10fbd0057e4315e4b2994a5
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-04-05 12:19:17 -0300

    instructions were missing

--- 3kyu/makeASpiral.py
+++ 3kyu/makeASpiral.py
@@ -1,3 +1,33 @@
+#Your task, is to create a NxN spiral with a given size.
+#
+#For example, spiral with size 5 should look like this:
+#
+#00000
+#....0
+#000.0
+#0...0
+#00000
+#and with the size 10:
+#
+#0000000000
+#.........0
+#00000000.0
+#0......0.0
+#0.0000.0.0
+#0.0..0.0.0
+#0.0....0.0
+#0.000000.0
+#0........0
+#0000000000
+#Return value should contain array of arrays, of 0 and 1, with the first row being composed of 1s. For example for given size 5 result should be:
+#
+#[[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+#Because of the edge-cases for tiny spirals, the size will be at least 5.
+#
+#General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.
+
+#My Solution
+
 #we can understand where we are in the array looking at row and col values
 
 def goRight(row, start, end): #define element = 0 from start, inclusive, till < end, left to right
