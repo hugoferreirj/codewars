f3d956602c1579df03219310319add5a44c2650f
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-04-21 15:46:44 -0300

    new changes in digits, almost there

--- 5kyu/digits.py
+++ 5kyu/digits.py
@@ -21,49 +21,59 @@ def possibleMatches(seqWithMatches, numberOfMatches):
 
 def guess(matches):
     global perms, lastSeq, sMatch, toTestU, testedV, oldMatches, testedU, answer
-    #precisa checar caso por exemplos matches va de 0 direto pra 2 ou 3
-    if sMatch and matches == oldMatches[-1] and len(toTestU) > 0:
+    if sMatch and matches == oldMatches[-1]:
+        print(toTestU)
         ind = testedU[-1].index(-1)
+        print(testedV[-1][ind])
+        print("not the match")
         for perm in perms[:]:
             if perm[ind] == testedV[-1][ind]:
                 perms.remove(perm)
-        nextSeq = toTestU.pop()
-        testedU.append(nextSeq)
-        return nextSeq
+            else: 
+                print(perm)
+        if len(toTestU) > 0:
+            testedU.append(toTestU[-1])
+            return toTestU.pop()
+        else:
+            sMatch = False
     elif sMatch and matches < oldMatches[-1]:
+        print(toTestU)
         ind = testedU[-1].index(-1)
         answer[ind] = testedV[-1][ind]
         print(answer)
         for perm in perms[:]:
-            if perm[ind] != testedV[-1][ind]:
-                perms.remove(perm)
-            else: 
-                print(perm)
-        toTestU = []
-        sMatch = False
+                if perm[ind] != testedV[-1][ind]:
+                    perms.remove(perm)
+                else: 
+                    print(perm)
+        if len(toTestU) > 0: #(len(answer) - answer.count(-1)) < oldMatches[-1]:
+            testedU.append(toTestU[-1])
+            return toTestU.pop()
+        else:
+            sMatch = False
     elif matches > -1:
         oldMatches.append(matches)
         perms = perms[1:]
         print("matches = " + str(matches) +  " oldMatches = " + str(oldMatches))
         print("testedV = " + str(testedV))
-        if matches == 0:
-            print("matches == 0")
+        if matches == 0 or (len(answer) - answer.count(-1)) == oldMatches[-1]:
+            print("new matches == 0")
             i = 1
             for perm in perms[:]:
-                if perm[0] == testedV[-1][0] or perm[1] == testedV[-1][1] or perm[2] == testedV[-1][2] or perm[3] == testedV[-1][3]:
+                if perm[0] == testedV[-1][0] != answer[0] or perm[1] == testedV[-1][1] != answer[1] or perm[2] == testedV[-1][2] != answer[2] or perm[3] == testedV[-1][3]  != answer[3]:
                     perms.remove(perm)
                 else:
                     print(str(i) + " " + str(perm))
                     i+=1
         elif (len(oldMatches) == 1 and matches > 0) or matches > oldMatches[-2]:
             i = 1
-            print("start printing Match > 1")
-            for perm in perms[:]:
-                if perm[0] != testedV[-1][0] and perm[1] != testedV[-1][1] and perm[2] != testedV[-1][2] and perm[3] != testedV[-1][3]:
-                    perms.remove(perm)
-                else:
-                    print(str(i) + " " + str(perm))
-                    i+=1
+            print("new matches > 0")
+            #for perm in perms[:]:
+            #    if perm[0] != testedV[-1][0] and perm[1] != testedV[-1][1] and perm[2] != testedV[-1][2] and perm[3] != testedV[-1][3]:
+            #        perms.remove(perm)
+            #    else:
+            #        print(str(i) + " " + str(perm))
+            #        i+=1
             possibleMatches(testedV[-1], matches)
             nextSeq = toTestU.pop()
             testedU.append(nextSeq)
