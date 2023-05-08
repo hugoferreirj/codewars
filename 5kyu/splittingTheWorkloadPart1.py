34c729d7be910db059e5f33e3626f4d669ed44b6
Hugo Ferreira <huugo.vieira49@gmail.com>
2023-05-08 13:45:53 -0300

    edited description of solution

--- 5kyu/splittingTheWorkloadPart1.py
+++ 5kyu/splittingTheWorkloadPart1.py
@@ -1,3 +1,26 @@
+#Imagine you have a number of jobs to execute. Your workers are not permanently connected to your network, so you have to distribute all your jobs in the beginning. Luckily, you know exactly how long each job is going to take.
+#
+#Let
+#
+#x = [2,3,4,6,8,2]
+#be the amount of time each of your jobs is going to take.
+#
+#Your task is to write a function splitlist(x), that will return two lists a and b, such that abs(sum(a)-sum(b)) is minimised.
+#
+#One possible solution to this example would be
+#
+#a=[2, 4, 6]
+#b=[3, 8, 2]
+#with abs(sum(a)-sum(b)) == 1.
+#
+#The order of the elements is not tested, just make sure that you minimise abs(sum(a)-sum(b)) and that sorted(a+b)==sorted(x).
+#
+#You may assume that len(x)<=15 and 0<=x[i]<=100.
+#
+#When done, please continue with part II!
+
+#My Solution
+
 import itertools as it
 
 def split_list(xs):
